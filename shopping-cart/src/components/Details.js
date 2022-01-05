6e6710c6202c3b2736bb73c64e861db8135e2223
Muhammad-Tahir <mtz5prif@gmail.com>
2022-01-05 15:49:20 +0100

    update

---
+++ shopping-cart/src/components/Details.js
@@ -0,0 +1,31 @@
+import Checked from "../assets/check.png";
+export default function Details() {
+  return (
+    <>
+      <div className="details-box" style={{ marginTop: "2rem" }}>
+        <span className="detail-number">a</span>
+        <div className="main-details" style={{ gap: "0.5rem" }}>
+          <span className="detail-title uppercase">
+            Login <img src={Checked} alt="checked" />{" "}
+          </span>
+          <p>Micheal Smith +806-445-4453</p>
+        </div>
+        <button className="change-btn uppercase fs-200">change</button>
+      </div>
+
+      <div className="details-box">
+        <span className="detail-number">b</span>
+        <div className="main-details" style={{ gap: "0.5rem" }}>
+          <span className="detail-title uppercase">
+            Shipping Address <img src={Checked} alt="checked" />{" "}
+          </span>
+          <p>
+            Brady Cooper, New Civil Colony, Salt Lake City, Utah, United States,
+            2971 Avenue.{" "}
+          </p>
+        </div>
+        <button className="change-btn uppercase fs-200">change</button>
+      </div>
+    </>
+  );
+}
