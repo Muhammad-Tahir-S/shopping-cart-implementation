64221b6470cec36bf0074072e688ba4090a68d41
Muhammad-Tahir <mtz5prif@gmail.com>
2022-01-11 21:34:59 +0100

    update 4

--- shopping-cart/src/components/DetailBox.js
+++ shopping-cart/src/components/DetailBox.js
@@ -1,9 +1,9 @@
 import Checked from "../assets/check.png";
 
-export default function DetailBox({ title, detail }) {
+export default function DetailBox({ title, detail, bullet }) {
   return (
     <div className="details-box">
-      <span className="detail-number">a</span>
+      <span className="detail-number">{bullet}</span>
       <div className="main-details">
         <span className="detail-title uppercase">
           {title} <img src={Checked} alt="checked" />{" "}
--- shopping-cart/src/components/Details.js
+++ shopping-cart/src/components/Details.js
@@ -2,17 +2,23 @@ import DetailBox from "./DetailBox";
 
 export default function Details() {
   const details = [
-    { title: "Login", detail: "Micheal Smith +806-445-4453" },
+    { title: "Login", detail: "Micheal Smith +806-445-4453", bullet: "a" },
     {
       title: "Shipping Address",
       detail: `Brady Cooper, New Civil Colony, Salt Lake City, Utah, United States,
   2971 Avenue.`,
+      bullet: "b",
     },
   ];
   return (
     <>
       {details.map((detail, index) => (
-        <DetailBox key={index} title={detail.title} detail={detail.detail} />
+        <DetailBox
+          key={index}
+          title={detail.title}
+          detail={detail.detail}
+          bullet={detail.bullet}
+        />
       ))}
     </>
   );
